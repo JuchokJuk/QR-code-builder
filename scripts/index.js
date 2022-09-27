fb6759d2352fe97d42a9405a83979818896bd91e
jukJuchok <juk.juchok@yandex.ru>
2022-09-27 15:26:50 +0300

    inital commit

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const OS = getOS();
+const userSelect = document.getElementById("user-select");
+
+if (OS === "iOS") {
+  location.href =
+    "https://apps.apple.com/ru/app/chrome-%D0%B1%D1%80%D0%B0%D1%83%D0%B7%D0%B5%D1%80-%D0%BE%D1%82-google/id535886823";
+} else if (OS === "Android") {
+  location.href =
+    "https://play.google.com/store/apps/details?id=com.android.chrome";
+} else if (OS === "Windows Phone") {
+  location.href =
+    "https://www.microsoft.com/en-in/p/google/9wzdncrfhx3w?activetab=pivot:overviewtab";
+} else {
+  userSelect.classList.remove("hidden");
+}
+
+function getOS() {
+  const userAgentString = navigator.userAgent;
+  if (
+    userAgentString.indexOf("iPhone") > -1 ||
+    userAgentString.indexOf("iPod") > -1 ||
+    userAgentString.indexOf("iPad") > -1
+  ) {
+    return "iOS";
+  } else if (/Android/.test(userAgentString)) {
+    return "Android";
+  } else if (/Windows Phone/.test(userAgentString)) {
+    return "Windows Phone";
+  } else {
+    return undefined;
+  }
+}
